e642b16f5e0b2b4241fc4689e886b0d65406614b
Lucia994 <l.summa94@gmail.com>
2025-06-03 09:18:54 +0200

    Create guest email list and prompt user to enter their email

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+console.log("Hello");
+/*Mail
+Crea una lista di email di invitati ad una festa. Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for. Non è consentito usare nessun metodo proprio degli array (come includes, per esempio). Si può fare? Certo che si basta ragionare un po’. Nota: Non è necessario provvedere alla validazione delle email
+Gioco dei dadi*/ 
+
+
+//Step
+
+//Step 1- Crea una lista di email di invitati
+const listPartyGuests = [
+    "mario.rossi@gmail.com",
+    "leonado_davinci@yahoo.com",
+    "tolkien_jkk@libero.it",
+    "matt_greoning@outlook.it",
+    "yayoi.kusama@gmail.com",
+    "jim_carrey@outlook.com",
+    "adam_sandler@gmail.com",
+
+]
